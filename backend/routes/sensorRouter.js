ee90410d9d25d3ffd343329e4006441f0e82d12d
Isak <it222hp@student.lnu.se>
2026-05-12 15:41:43 +0200

    add: sensorRouter base

---
+++ backend/routes/sensorRouter.js
@@ -0,0 +1,10 @@
+import express from 'express'
+import { SensorController } from '../controllers/sensorController.js'
+
+export const router = express.Router()
+
+const controller = new SensorController()
+
+router.get('/', (req, res, next) => controller.index(req, res, next))
+
+
