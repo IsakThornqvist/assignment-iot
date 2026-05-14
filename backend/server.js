9d32c4df47cd0618dea03dd731aa321c5346910c
Isak <it222hp@student.lnu.se>
2026-05-14 14:25:56 +0200

    fix: route import in server

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ import connectMqttBroker from './mqttClient.js'
 import cors from 'cors'
 import { dirname, join } from 'node:path'
 import { fileURLToPath } from 'node:url'
-import router from './routes/router.js'
+import { router } from '../backend/routes/router.js'
 
 const directoryFullName = dirname(fileURLToPath(import.meta.url))
 
