731fda0ad1613d503a52fde5492c89b6709c7e89
Isak <it222hp@student.lnu.se>
2026-05-14 14:03:08 +0200

    add: async function getReadings that finds the last 50 readings and sort them by newest first (with error handling)

--- backend/controllers/sensorController.js
+++ backend/controllers/sensorController.js
@@ -4,9 +4,17 @@ import SensorReading from "../models/sensorReadingsModel.js"
 export class SensorController {
 
 
-    async getReadings () {
-
+export async getReadings(req, res, next) {
+    try {
+        const readings = await SensorReading.find()
+            .sort({ createdAt: -1 })
+            .limit(50)
+        
+        res.json(readings)
+    } catch (error) {
+        next(error)
     }
+}
 
-    
+
 }
