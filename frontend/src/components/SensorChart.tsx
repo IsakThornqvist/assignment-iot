775cbab868eea3f071e641b1c52fd018cada390c
Isak <it222hp@student.lnu.se>
2026-05-18 14:16:56 +0200

    add: imports for recharts and SensorChartProps definition

---
+++ frontend/src/components/SensorChart.tsx
@@ -0,0 +1,23 @@
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts'
+import type { SensorReading } from '../types/index'
+
+
+
+
+interface SensorChartProps {
+
+    readings: SensorReading[]
+
+}
+
+const SensorChart = ({ readings }: SensorChartProps) => {
+    
+
+return (
+
+
+    
+)
+}
+
+
