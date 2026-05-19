9dd5848b6d5f4cd77fc6efae8eba4c4e6650c49d
Isak <it222hp@student.lnu.se>
2026-05-19 15:21:02 +0200

    add: use sensor data hook with mqtt and rest

--- frontend/src/hooks/useSensorData.ts
+++ frontend/src/hooks/useSensorData.ts
@@ -1,11 +1,9 @@
 import { useEffect, useState } from "react"
-import { SensorReading } from "../types"
+import type { SensorReading } from "../types/index"
 import axios from 'axios'
 import mqtt from 'mqtt'
 
-interface SensorResponse {
-    allReadings: SensorReading[]
-}
+
 
 export function useAllSensorData () {
     const [readings, setReadings] = useState<SensorReading[]>([])
@@ -15,5 +13,49 @@ export function useAllSensorData () {
     const [currentHumidity, setHumidity] = useState<number | null>(null)
 
 
-    
+useEffect(() => {
+    const fetchData = async () => {
+        try {
+            const response = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/api/readings`)
+            setReadings(response.data.reverse())
+        } catch (error) {
+            console.error(error)
+        }
+    }
+    fetchData()
+}, [])
+
+useEffect(() => {
+    const client = mqtt.connect(import.meta.env.VITE_MQTT_BROKER, {
+        username: import.meta.env.VITE_MQTT_USERNAME,
+        password: import.meta.env.VITE_MQTT_PASSWORD,
+        clientId: `dashboard_${Math.random().toString(16).slice(2)}`
+    })
+
+    client.on("connect", () => {
+        client.subscribe("lnu/iot/it222hp/sensor")
+    })
+
+client.on("message", (topic, message) => {
+    console.log("Live message received:", message.toString())
+    const payload = JSON.parse(message.toString())
+    const newReading = {
+        ...payload,
+        createdAt: new Date().toISOString()
+    }
+    setTemperature(payload.temperature)
+    setHumidity(payload.humidity)
+    setReadings(prev => [...prev, newReading])
+})
+
+    client.on("error", (error) => {
+        console.error(error)
+    })
+
+    return () => {
+        client.end()
+    }
+}, [])
+
+return { readings, currentTemperature, currentHumidity}
 }
