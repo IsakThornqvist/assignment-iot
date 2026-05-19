7dc6d6024ca06ea8f25892b88a516dfc2bee8611
Isak <it222hp@student.lnu.se>
2026-05-19 15:41:30 +0200

    add: led control import and definition to app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,19 @@
 import { useAllSensorData } from './hooks/useSensorData'
+import { useLedControl } from './hooks/useLedControl'
 import SensorValue from './components/SensorValue'
 import SensorChart from './components/SensorChart'
 import './App.css'
+import LedControls from './components/LedControls'
 
 function App() {
   const { readings, currentTemperature, currentHumidity } = useAllSensorData()
+  const { ledState, handleLedToggle } = useLedControl()
 
   return (
     <div>
       <h1>IoT Dashboard</h1>
       <SensorValue temperature={currentTemperature} humidity={currentHumidity} />
+      <LedControls ledState={ledState} onToggle={handleLedToggle} />
       <SensorChart readings={readings} />
     </div>
   )
